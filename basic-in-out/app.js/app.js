3afa1a5b675d1683147de12866daed08e4c3c031
ZawHlaingPhyoTsuki <tro2233zhp@gmail.com>
2024-09-13 00:07:06 +0700

    DOM Day 2 (Nov Day 39)

--- basic-in-out/app.js/app.js
+++ basic-in-out/app.js/app.js
@@ -1,57 +1,75 @@
-// const age = window.prompt("How old are you?");
+// // const age = window.prompt("How old are you?");
 
-// const birthYear = 2024 - parseInt(age);
+// // const birthYear = 2024 - parseInt(age);
 
-// window.alert(`You were born in ${birthYear}`);
+// // window.alert(`You were born in ${birthYear}`);
 
-//////////////
+// //////////////
 
-// const answer = window.confirm("Are you finish dinner ?")
-// console.log(answer ? "Yes, I finished" : "No, not yet");
+// // const answer = window.confirm("Are you finish dinner ?")
+// // console.log(answer ? "Yes, I finished" : "No, not yet");
 
-//select an element
+// //select an element
 
 
-// const heading = document.getElementById("heading")
+// // const heading = document.getElementById("heading")
+// // const textInput = document.getElementById("textInput");
+// // const listGroup = document.getElementById("listGroup")
+// // const btn = document.getElementById("btn");
+
+// // console.log(heading);
+// // console.log(heading.innerHTML);
+// // console.log(heading.innerText);
+
+// // console.log(listGroup);
+// // console.log(listGroup.innerHTML); // not the same
+// // console.log(listGroup.innerText); // not the same
+
+// // console.log(textInput);
+// // console.log(btn);
+// //------------------------------------
+// // small software
+
+// // 1. select an element
+// const heading = document.getElementById("heading");
 // const textInput = document.getElementById("textInput");
-// const listGroup = document.getElementById("listGroup")
+// const listGroup = document.getElementById("listGroup");
 // const btn = document.getElementById("btn");
 
-// console.log(heading);
-// console.log(heading.innerHTML);
-// console.log(heading.innerText);
+// ///// heading.innerText = textInput.value 
 
-// console.log(listGroup);
-// console.log(listGroup.innerHTML); // not the same
-// console.log(listGroup.innerText); // not the same
+// // 2. set
+// function changeTitle() {
+//     heading.innerText = textInput.value;
 
-// console.log(textInput);
-// console.log(btn);
-//------------------------------------
-// small software
+// // 3. clear
+//     textInput.value = '';
+// }
 
-// 1. select an element
-const heading = document.getElementById("heading");
-const textInput = document.getElementById("textInput");
-const listGroup = document.getElementById("listGroup");
-const btn = document.getElementById("btn");
+// // 4. event listen
+// // btn.onclick = changeTitle; // no parenthesis  !!!!!
+// // btn.addEventListener("click", changeTitle); // another method
+// // another method is adding attribute to html tag (onclick = "changeTitle") // but must have ()
 
-///// heading.innerText = textInput.value 
+// //------------------------------------
 
-// 2. set
-function changeTitle() {
-    heading.innerText = textInput.value;
+//Day 39
 
-// 3. clear
-    textInput.value = '';
+const heading = document.querySelector("#heading")
+const btn = document.querySelector("#btn")
+const headingLarger = document.querySelector("#headingLarger")
+const headingSmaller = document.querySelector("#headingSmaller");
+
+btn.onclick = () => {
+    heading.style.color = "green"
+    heading.style.backgroundColor = "gray"
 }
 
-// 4. event listen
-// btn.onclick = changeTitle; // no parenthesis  !!!!!
-// btn.addEventListener("click", changeTitle); // another method
-// another method is adding attribute to html tag (onclick = "changeTitle") // but must have ()
+headingLarger.onclick = () => {
+    const current = parseInt(heading.style.fontSize)
+    heading.style.fontSize = `${current + 5}px`
+}
 
-//------------------------------------
-
-
-
+headingSmaller.onclick = () => {
+    heading.style.fontSize = `${parseInt(heading.style.fontSize) - 5}px`
+}
---
+++ basic-in-out/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html","nav.html","calc.html","./app.js/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
